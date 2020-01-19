eee2ef41bc6b4462502a11418ee90fef1b412455
ymcatar <ymcatar@gmail.com>
2020-01-19 11:46:15 +0800

    add key to set rgb color

--- keyboards/dztech/dz60rgb/keymaps/isaac/keymap.c
+++ keyboards/dztech/dz60rgb/keymaps/isaac/keymap.c
@@ -1,5 +1,18 @@
 #include QMK_KEYBOARD_H
 
+enum dz60rgb_keycodes {
+	USR_CRM = SAFE_RANGE,
+	USR_PRP,
+	USR_RED,
+	USR_GRN,
+	USR_BLU,
+	USR_CYN,
+	USR_MGT,
+	USR_YEL,
+	USR_KEY,
+	USR_WHT,
+};;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[0] = LAYOUT(
@@ -21,6 +34,75 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS,			KC_F1,		KC_F2,		KC_F3,		KC_F4,		KC_F5,		KC_F6,		KC_F7,		KC_F8,		KC_F9,		KC_F10,				KC_F11,		KC_F12,			KC_DEL,
 		KC_TRNS,			RGB_TOG,	KC_TRNS,	RGB_HUI,	RGB_HUD,	RGB_SAI,	RGB_SAD,	RGB_VAI,	RGB_VAD,	RGB_MOD,	KC_TRNS,			KC_TRNS,	KC_TRNS,		RESET,
 		KC_TRNS,			KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	RGB_SPI,	RGB_SPD,	KC_TRNS,			KC_TRNS,	EEP_RST,
-		KC_TRNS,			KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,			KC_TRNS,	KC_TRNS,
+		KC_TRNS,			USR_CRM,	USR_PRP,	USR_RED,	USR_GRN,	USR_BLU,	USR_CYN,	USR_MGT,	USR_YEL,	USR_KEY,	USR_WHT,			KC_TRNS,	KC_TRNS,
 		KC_TRNS,			KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS,	KC_TRNS)
 };
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record)
+{
+	switch (keycode) {
+
+		case USR_CRM:
+			if (record->event.pressed) {
+				rgb_matrix_sethsv(32, 160, 180);
+			}
+			return false;
+
+		case USR_PRP:
+			if (record->event.pressed) {
+				rgb_matrix_sethsv(192, 112, 180);
+			}
+			return false;
+
+		case USR_RED:
+			if (record->event.pressed) {
+				rgb_matrix_sethsv(0, 255, 180);
+			}
+			return false;
+
+		case USR_GRN:
+			if (record->event.pressed) {
+				rgb_matrix_sethsv(88, 255, 180);
+			}
+			return false;
+
+		case USR_BLU:
+			if (record->event.pressed) {
+				rgb_matrix_sethsv(168, 255, 180);
+			}
+			return false;
+
+		case USR_CYN:
+			if (record->event.pressed) {
+				rgb_matrix_sethsv(128, 255, 180);
+			}
+			return false;
+
+		case USR_MGT:
+			if (record->event.pressed) {
+				rgb_matrix_sethsv(216, 255, 180);
+			}
+			return false;
+
+		case USR_YEL:
+			if (record->event.pressed) {
+				rgb_matrix_sethsv(40, 255, 180);
+			}
+			return false;
+
+		case USR_KEY:
+			if (record->event.pressed) {
+				rgb_matrix_sethsv(0, 0, 0);
+			}
+			return false;
+
+		case USR_WHT:
+			if (record->event.pressed) {
+				rgb_matrix_sethsv(80, 128, 180);
+			}
+			return false;
+
+		default:
+			return true;
+	}
+}
